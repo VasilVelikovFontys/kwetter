05f905e6f1f10e31963481f8302682cc016b117c
Vasil Velikov <vasil.velikov@orbisagency.com>
2022-03-25 13:16:35 +0200

    added mentioning posts route to gateway

--- src/gateway/routes/posts.js
+++ src/gateway/routes/posts.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 const {
     POSTS_SERVICE_HOST,
-    POSTS_SERVICE_PORT
+    POSTS_SERVICE_PORT,
+    MENTIONING_POSTS_SERVICE_HOST,
+    MENTIONING_POSTS_SERVICE_PORT
 } = process.env;
 
 router.get('/posts', async (req, res) => {
@@ -26,5 +28,14 @@ router.post('/posts', async (req, res) => {
         res.status(202).send(err);
     }
 });
+
+router.get('/posts/mentioning', async (req, res) => {
+    try {
+        const response = await axios.get(`${MENTIONING_POSTS_SERVICE_HOST}:${MENTIONING_POSTS_SERVICE_PORT}/mentioning-posts/3s05d8Cj80ZzOYFOrC5wtERhlCt2`);
+        res.status(200).send(response.data);
+    } catch (err) {
+        res.status(200).send(err);
+    }
+})
 
 module.exports = router;
