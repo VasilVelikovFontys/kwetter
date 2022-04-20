0a1cb3bf1dcb547ca175810e82c9306d935d1c46
Vasil Velikov <vasil.velikov@orbisagency.com>
2022-04-20 18:02:08 +0300

    fixed code smell

--- src/trends/src/routes/trends.js
+++ src/trends/src/routes/trends.js
@@ -3,7 +3,7 @@ const express = require("express");
 const createTrendsRouter = database => {
     const router = express.Router();
 
-    router.get('/trends', async (req, res) => {
+    router.get('/trends', async (_req, res) => {
         const trends = await database.getTrends();
 
         res.status(200).send({trends});
